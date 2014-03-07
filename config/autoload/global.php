6e5f57019745eb0479cf14f01ad632bc3951eff7
gabriella <gnurena@phantasia.pe>
2014-03-07 17:09:29 -0500

    archivo de configuracion con bd

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,16 +1,18 @@
 <?php
-/**
- * Global Configuration Override
- *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
 
 return array(
-    // ...
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
+    'db' => array(
+        'username' => 'root',
+        'password' => '*sql$$cr1xus*',
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=db_cusquena2013;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'utf8\''
+        ),
+    )
 );
